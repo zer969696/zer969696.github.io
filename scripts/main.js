315d4f13ab039543d22edf98babd2be0843e2bb8
evgeniy-evzerov <evgeniy.evzerov@vistar.su>
2017-01-31 12:38:13 +0300

    commit-message

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  var currentUrl = window.location.href;
-  
-  if (+currentUrl.split('&').length === 4) {
-    var res = currentUrl.indexOf('state=');
-    var state = currentUrl.substr(res, 12).split('=')[1];
-    
-    if (state === '123456') {
+//  var currentUrl = window.location.href;
+//  
+//  if (+currentUrl.split('&').length === 4) {
+//    var res = currentUrl.indexOf('state=');
+//    var state = currentUrl.substr(res, 12).split('=')[1];
+//    
+//    if (state === '123456') {
       VK.init(function() {
            alert('ok');
            // Your code here
@@ -13,10 +13,10 @@ $(document).ready(function() {
            // API initialization failed
            // Can reload page here
       }, '5.62'); 
-    } else {
-      alert("ne ok");
-    }
-  } else {
-    window.location.href = "https://oauth.vk.com/authorize?client_id=5850378 &display=page&redirect_uri=https://zer969696.github.io&scope=audio&response_type=token&v=5.62&state=123456";
-  }
+//    } else {
+//      alert("ne ok");
+//    }
+//  } else {
+//    window.location.href = "https://oauth.vk.com/authorize?client_id=5850378 &display=page&redirect_uri=https://zer969696.github.io&scope=audio&response_type=token&v=5.62&state=123456";
+//  }
 })
