739bcca808c572824cc38d67e71c1fcab3296342
evgeniy-evzerov <evgeniy.evzerov@vistar.su>
2017-01-31 11:58:08 +0300

    test-vk-auth

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-  window.location.href = "vk.com";
+  window.location.href = "https://oauth.vk.com/authorize?client_id=5850378 &display=page&redirect_uri=https://zer969696.github.io&scope=audio&response_type=token&v=5.62&state=123456";
 })
